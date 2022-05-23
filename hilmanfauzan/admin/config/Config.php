12fa252a829e450d3bc4956918ddbaf975940ae6
Sofakartika <192010324@smkn4-tsm.sch.id>
2022-05-23 16:05:27 +0700

    hai

---
+++ hilmanfauzan/admin/config/Config.php
@@ -0,0 +1,9 @@
+<?php
+
+define('baseurl','http://localhost/03079/hilmanfauzan/public');
+
+//DB
+define('DB_HOST','localhost');
+define('DB_USER','root');
+define('DB_PASS','');
+define('DB_NAME','03079');
---
+++ hilmanfauzan/admin/core/koneksi.php
@@ -0,0 +1,5 @@
+<?php
+$conn = mysqli_connect('localhost', 'root', '','03079');
+return $conn;
+
+?>
